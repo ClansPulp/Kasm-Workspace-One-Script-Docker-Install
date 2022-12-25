84f61f38b2077478885c54dc9d204508f76c72e9
ClansPulp <121213510+ClansPulp@users.noreply.github.com>
2022-12-25 23:08:53 +1100

    Update kasm_setup.sh

--- kasm_workspace/kasm_setup.sh
+++ kasm_workspace/kasm_setup.sh
@@ -1,18 +1,18 @@
 echo 'Creating swap partition'
-sudo dd if=/dev/zero bs=1M count=1024 of=/mnt/1GiB.swap
-sudo chmod 600 /mnt/1GiB.swap
-sudo mkswap /mnt/1GiB.swap
-sudo swapon /mnt/1GiB.swap
-echo ‘/mnt/1GiB.swap swap swap defaults 0 0’ | sudo tee -a /etc/fstab
-sudo apt update
-sudo apt install curl -y
-sudo apt install docker.io -y
+dd if=/dev/zero bs=1M count=1024 of=/mnt/1GiB.swap
+chmod 600 /mnt/1GiB.swap
+mkswap /mnt/1GiB.swap
+swapon /mnt/1GiB.swap
+echo ‘/mnt/1GiB.swap swap swap defaults 0 0’ | tee -a /etc/fstab
+apt update
+apt install curl -y
+apt install docker.io -y
 echo 'Downloading and isntalling Kasm Workspace'
 wget https://kasm-static-content.s3.amazonaws.com/kasm_release_1.12.0.d4fd8a.tar.gz
 tar -xf kasm_release*.tar.gz
 echo 'This installation will take some time, please be patient. Credentials will be 
 posted once complete'
-sudo bash kasm_release/install.sh
+bash kasm_release/install.sh
 y
 echo 'Installation complete'
 hostname -i
